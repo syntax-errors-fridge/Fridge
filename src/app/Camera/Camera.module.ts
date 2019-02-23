89c0326d5b824125dbd265e286fab5ee8f21bb34
Yeetus Mightus <thesiddyshow@gmail.com>
2019-02-23 14:55:25 -0500

    camera shet

--- src/app/Camera/Camera.module.ts
+++ src/app/Camera/Camera.module.ts
@@ -4,20 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 
-import { HomePage } from './home.page';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
-@NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: HomePage
-      }
-    ])
-  ],
-  declarations: [HomePage]
-})
-export class HomePageModule {}
+constructor(private camera: Camera) { }
+
+...
+
+
+const options: CameraOptions = {
+  quality: 100,
+  destinationType: this.camera.DestinationType.FILE_URI,
+  encodingType: this.camera.EncodingType.JPEG,
+  mediaType: this.camera.MediaType.PICTURE
+}
+
+this.camera.getPicture(options).then((imageData) => {
+ // imageData is either a base64 encoded string or a file URI
+ // If it's base64 (DATA_URL):
+ let base64Image = 'data:image/jpeg;base64,' + imageData;
+}, (err) => {
+ // Handle error
+});
