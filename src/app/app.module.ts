3874292d1f7229b89a1c3e71c00db83f89bacfa3
JuneHyk <kongjj@ymail.com>
2019-03-12 20:39:55 -0400

    'urmoderghee'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { BarcodeScanner } from '@ionic-native/barcode-scanner';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -21,7 +20,6 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    BarcodeScanner,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { BarcodeScanner, BarcodeScannerOptions } from '@ionic-native/barcode-scanner';
 import { NavController } from '@ionic/angular';
 
 @Component({
@@ -8,28 +7,5 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  options: BarcodeScannerOptions;
-  encodText:string='';
-  encodedData: any={};
-  scannedData: any={};
-  constructor(public navCtrl: NavController, public scanner:BarcodeScanner) {
-
-  }
-  scan(){
-    this.options= {
-      prompt: "Scan your barcode"
-    };
-    this.scanner.scan(this.options).then((data) => {
-      this.scannedData = data;
-    }, (err) => {
-      console.log('Error :', err);
-    })
-  }
-  encode(){
-    this.scanner.encode(this.scanner.Encode.TEXT_TYPE, this.encodText).then((data) => {
-      this.encodedData = data;
-    }, (err) => {
-      console.log('Error :', err);
-    })
-  }
+  
 }
